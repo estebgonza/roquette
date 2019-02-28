dff2ed85675a5a2e947cce0aebaa5d52e058e6da
Esteban <esteban@indexima.com>
2019-02-28 17:03:01 +0100

    unit test

---
+++ roqclient/roqclient_test.go
@@ -0,0 +1,35 @@
+package roqclient
+
+import "testing"
+
+func TestDriverExist(t *testing.T) {
+	type driver struct {
+		name     string
+		expected string
+	}
+	drivers := []driver{
+		driver{
+			name:     "hive",
+			expected: "",
+		},
+		driver{
+			name:     "postgres",
+			expected: "type does'nt exist or not supported",
+		},
+		driver{
+			name:     "",
+			expected: "driver name not specified",
+		},
+	}
+
+	for _, driver := range drivers {
+		_, err := getClientByType(driver.name)
+		errorMessage := ""
+		if err != nil {
+			errorMessage = err.Error()
+		}
+		if driver.expected != errorMessage {
+			t.Errorf("expected: %s, actual: %s", driver.expected, errorMessage)
+		}
+	}
+}
